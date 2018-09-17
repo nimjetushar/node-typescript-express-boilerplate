b4c066a674353c61fb4bb1663aac0920f7070300
Tushar <tusharnimje21@gmail.com>
2018-09-17 22:07:31 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import http from "http";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import { AddressInfo } from "net";
+
+import { config } from "./config";
+
+import initializeDb from "./db";
+import api from "./controllers";
+
+let app = express();
+const server = http.createServer(app);
+
+// logger
+app.use(morgan("dev"));
+
+// 3rd party middleware
+app.use(
+  cors({
+    exposedHeaders: config.corsHeaders
+  })
+);
+
+app.use(
+  bodyParser.json({
+    limit: config.bodyLimit
+  })
+);
+
+// connect to db
+initializeDb(config).then(db => {
+  // api router
+  app.use("/api", api());
+
+  server.listen(config.port, () => {
+    const serverDetail = server.address() as AddressInfo;
+    console.log(`Started on port ${serverDetail.port}`);
+  });
+});
+
+export default app;
